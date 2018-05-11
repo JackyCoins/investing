11e1bcfc3909e5b423fe7409cb3c4ba46b3afa6b
Nikita Rusanov <rusanov.n.d@gmail.com>
2018-05-11 21:27:35 +0300

    Finished 2 stage

--- .next/manifest.js
+++ .next/manifest.js
@@ -90,7 +90,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "6c5efe8cafa004a10ecc"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "b5536f7fdf7b1d3ca051"; // eslint-disable-line no-unused-vars
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
@@ -755,7 +755,7 @@
 /******/ 		if (__webpack_require__.nc) {
 /******/ 			script.setAttribute("nonce", __webpack_require__.nc);
 /******/ 		}
-/******/ 		script.src = __webpack_require__.p + "" + ({"0":"main.js","1":"bundles/pages/_error.js","4":"bundles/pages/applications.js","5":"bundles/pages/investments.js"}[chunkId]||chunkId) + "-" + {"0":"319fd5236d914c7211f2","1":"9a3a532253314e1f0220","4":"74fc18e914d970503374","5":"4d29ced65b19d3fba132"}[chunkId] + ".js";
+/******/ 		script.src = __webpack_require__.p + "" + ({"0":"main.js","1":"bundles/pages/_error.js","4":"bundles/pages/investments.js","5":"bundles/pages/applications.js"}[chunkId]||chunkId) + "-" + {"0":"319fd5236d914c7211f2","1":"9a3a532253314e1f0220","4":"2a5b9632acec05e131b8","5":"f8d0150a4882d92ebd74"}[chunkId] + ".js";
 /******/ 		var timeout = setTimeout(onScriptComplete, 120000);
 /******/ 		script.onerror = script.onload = onScriptComplete;
 /******/ 		function onScriptComplete() {
